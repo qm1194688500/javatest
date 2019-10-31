6514b96cfa90d1e97aa11311602000375e4a01c5
qm1194688500 <1194688500@qq.com>
2019-10-31 22:33:05 +0800

    练习

---
+++ 2019.10.31/src/demo1.java
@@ -0,0 +1,16 @@
+public class demo1 {
+    public static void main(String[] args) {
+       /* int sum = 0;
+        for (int i = 1; i <=100 ; i++) {
+            sum+=i;
+        }
+       int sum = 0;
+       int i = 1;int n = 100;
+       sum = (i + n)*n/2;
+       int sum = 0;
+       int i = 0; int n = 100;
+       sum = n*(n+1)/2;
+        System.out.println(sum);*/
+
+    }
+}
