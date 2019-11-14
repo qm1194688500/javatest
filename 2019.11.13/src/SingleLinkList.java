b2b98e4432f57a2cece76638d67fe138dac215ee
qm1194688500 <1194688500@qq.com>
2019-11-14 23:36:17 +0800

    链表

--- 2019.11.13/src/SingleLinkList.java
+++ 2019.11.13/src/SingleLinkList.java
@@ -34,15 +34,94 @@ public class SingleLinkList {
             cur.next=node;
         }
     }
-    //插入到index位置
-
+    //任意位置插入,第一个数据节点为0号下标
+    private ListNode searchIndex(int index){
+        //找到index-1位置
+        ListNode pre = this.head;
+        int count = 0;
+        while (count<index-1){
+            pre = pre.next;
+            count++;
+        }
+        return pre;
+    }
+    public boolean addIndex(int index,int data){
+        ListNode node = new ListNode(data);
+        ListNode pre = searchIndex(index);
+        if (index==0){
+            node.next=this.head;
+            this.head=node;
+            return true;
+        }
+        if (index<0||index>getLength()){
+            System.out.println("输入位置不合法");
+            return false;
+        }
+        node.next=pre.next;
+        pre.next=node;
+        return false;
+    }
     //获取链表长度
+    public int getLength(){
+        ListNode cur = this.head;
+        int count = 0;
+        while (cur!=null){
+            cur=cur.next;
+            count++;
+        }
+        return count;
+    }
     //打印链表数据
+    public void display(){
+        ListNode cur = this.head;
+        while (cur!=null){
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
     //查找是否存在关键字key在链表中
-    //删除第一次出现关键字key的节点
-    //删除所有出现关键字key的节点
-    // 在 pos 位置新增元素
-    // 获取 pos 位置的元素
-    // 给 pos 位置的元素设为 value
+    public boolean searchKey(int key){
+        ListNode cur = this.head;
+        while (cur!=null){
+            if (cur.data==key){
+                System.out.println("有关键字key");
+                return true;
+            }
+            cur = cur.next;
+            }
+            return false;
+        }
+        //删除第一次出现关键字key的节点
+    private  ListNode searchPrev(int key){
+        ListNode cur = this.head;
+        while (cur.next!=null){
+            if (cur.next.data==key){
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+    public void deleteKey(int key){
+        if (this.head.data==key){
+            this.head=this.head.next;
+            return;
+        }
+        ListNode pre = searchPrev(key);
+       if (pre==null){
+           System.out.println("没有你要删除的结点");
+       }
+      ListNode del = pre.next;
+       pre.next=del.next;
 
-}
+    }
+    //删除所有出现关键字key的节点
+    public void deleteAllkey(int key){
+        
+    }
+    }
+
+
+
+
--- 2019.11.13/src/demo1.java
+++ 2019.11.13/src/demo1.java
@@ -1,5 +1,20 @@
 public class demo1 {
     public static void main(String[] args) {
+        SingleLinkList list = new SingleLinkList();
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.display();
+        list.addIndex(0,0);
+        list.display();
+        list.addIndex(7,0);
+        list.display();
+        list.searchKey(0);
+        list.deleteKey(0);
+        list.display();
 
     }
 }
