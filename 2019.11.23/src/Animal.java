c78755d6c56f8afe71f948af90a8ccd58db43ac1
qm1194688500 <1194688500@qq.com>
2019-11-23 23:48:49 +0800

    抽象类和对象

---
+++ 2019.11.23/src/Animal.java
@@ -0,0 +1,68 @@
+class Animal {
+    protected String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+}
+interface IFlying {
+    void fly();
+}
+interface IRunning {
+    void run();
+}
+interface ISwimming {
+    void swim();
+}
+//猫是会跑的
+class Cat extends Animal implements IRunning {
+    public Cat(String name) {
+        super(name);
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在用四条腿跑");
+    }
+}
+//鱼是会游泳的
+class Fish extends Animal implements ISwimming{
+    public Fish(String name){
+        super(name);
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(this.name + "正在用尾巴游");
+    }
+}
+//青蛙既能跑又能游
+class Frog extends Animal implements IRunning,ISwimming{
+    public Frog(String name){
+        super(name);
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在往前跳");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(this.name + "正在蹬腿游泳");
+    }
+}
+class Duck extends Animal implements IRunning, ISwimming, IFlying {
+    public Duck(String name) {
+        super(name);
+    }
+    @Override
+    public void fly() {
+        System.out.println(this.name + "正在用翅膀飞");
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "正在用两条腿跑");
+    }
+    @Override
+    public void swim() {
+        System.out.println(this.name + "正在漂在水上");
+    }
+}
