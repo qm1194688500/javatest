e60a17595feb559a0f65f1534998df0a1e309920
qm1194688500 <1194688500@qq.com>
2019-11-30 23:29:41 +0800

    修改

--- 2019.11.25/src/Main.java
+++ 2019.11.25/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入姓名：");
         String name = scanner.nextLine();
-        System.out.println("请输入你的身份：1-》管理员 0：普通用户");
+        System.out.println("请输入你的身份：1：管理员 0：普通用户");
         int who = scanner.nextInt();
         if(who == 1) {
             return new Admin(name);
