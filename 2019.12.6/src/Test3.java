a18a88e2b891999bb0076e4306ae002b681772b6
qm1194688500 <1194688500@qq.com>
2019-12-09 20:46:17 +0800

    修改

--- 2019.12.6/src/Test3.java
+++ 2019.12.6/src/Test3.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
+
 public class Test3 {
+
     public static void main(String[] args) {
         int[] arr = {0,1,2,3,4,5};
         int x = 6;
-
+        int[] ret = twoSum(arr,6);
+        System.out.println(Arrays.toString(ret));
     }
-    public int[] twoSum(int[] nums, int target) {
-
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for (int j = i+1; j <nums.length ; j++) {
+                if (nums[i] == target-nums[j]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("没有合适的值");
     }
 }
