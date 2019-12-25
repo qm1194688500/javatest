7c5ade5e3ad825e7889d86f76acbb38a1bd5dbc2
qm1194688500 <1194688500@qq.com>
2019-12-25 23:48:43 +0800

    排序

--- 2019.12.24/src/Test.java
+++ 2019.12.24/src/Test.java
@@ -16,6 +16,24 @@ public class Test {
         }
 
     }
+    public static void shellSort(int[] array) {
+        int gap = array.length;
+        while (gap > 1) {
+            insertSortGap(array, gap);
+            gap = (gap / 3) + 1; // OR gap = gap / 2;
+        }
+        insertSortGap(array, 1);
+    }
+    private static void insertSortGap(int[] array, int gap) {
+        for (int i = 1; i < array.length; i++) {
+            int v = array[i];
+            int j = i - gap;
+            for (; j >= 0 && array[j] > v; j -= gap) {
+                array[j + gap] = array[j];
+            }
+            array[j + gap] = v;
+        }
+    }
     public static void selectSort(int[] array){
         for (int i = 0; i <array.length ; i++) {
             for (int j = i+1; j <array.length ; j++) {
