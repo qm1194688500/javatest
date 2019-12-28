397eb7e4999c13abce2e8322dc711ce10135e263
qm1194688500 <1194688500@qq.com>
2019-12-28 22:50:20 +0800

    排序

--- 2019.12.23/src/Test.java
+++ 2019.12.23/src/Test.java
@@ -1,4 +1,5 @@
 
+
 public class Test {
     public static int partion(int[] array,int start,int end){
         int tmp = array[start];
@@ -24,7 +25,20 @@ public class Test {
         return start;
     }
     public static void bobbleSort(int[] array){
-
+        boolean flg = false;
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    flg = true;
+                }
+            }
+            if(!flg) {
+                break;
+            }
+        }
     }
     public static void main(String[] args) {
 
