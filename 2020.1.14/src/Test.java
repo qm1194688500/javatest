45da1e821ccd4c5f58b61de62f68e6875217e3fe
qm1194688500 <1194688500@qq.com>
2020-01-25 21:17:15 +0800

    练习

--- 2020.1.14/src/Test.java
+++ 2020.1.14/src/Test.java
@@ -11,9 +11,16 @@ public class Test {
         System.out.println(Arrays.toString(array));*/
         /*int[] array = {1,3,4,5,6};
         int n = searchInsert(array,2);*/
-        String s = "a ";
+        /*String s = "a ";
         int x = lengthOfLastWord(s);
-        System.out.println(x);
+        System.out.println(x);*/
+        int[] sum1 = {1,2,3,0,0,0};
+        int[] sum2 = {4,5,6};
+        merge(sum1,3,sum2,3);
+        for (int x:sum1
+             ) {
+            System.out.println(x);
+        }
     }
     public static void mystery (int x) {
         System.out.print(x % 10);
@@ -104,5 +111,77 @@ public class Test {
             }
         }
         return length;
+    }
+    //给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }
+        }
+        for (int t = 0; t < j + 1; t++) {
+            nums1[t] = nums2[t];
+        }
+    }
+    //给定一个整数数组，判断是否存在重复元素。
+    //
+    //如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i <nums.length-1 ; i++) {
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    //你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+    //你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
+    public boolean isLongPressedName(String name, String typed) {
+        int len_n = name.length();
+        int len_t = typed.length();
+        if (len_t<len_n){
+            return false;
+        }
+        int i = 0;
+        int j = 0;
+        while (i<len_n&&j<len_t){
+            if (name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }else if (j>0&&typed.charAt(j)==typed.charAt(j-1)){
+                j++;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    //给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i <A.length ; i++) {
+            A[i] = A[i]*A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+    //给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+    public String reverseOnlyLetters(String S) {
+        int len = S.length();
+        char[] temp = new char[len];
+        for (int i = 0; i <len ; i++) {
+            if (Character.isLowerCase(S.charAt(i))){
+                temp[]
+            }
+        }
     }
 }
