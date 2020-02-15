5e3b2abe7dbcbc33cffede7b27c9258143bbd534
qm1194688500 <1194688500@qq.com>
2020-02-15 23:22:32 +0800

    练习

--- OSMonitor/src/com/bitedu/gui/OSMonitorController.java
+++ OSMonitor/src/com/bitedu/gui/OSMonitorController.java
@@ -2,9 +2,16 @@ package com.bitedu.gui;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.TreeTableView;
 
 public class OSMonitorController {
+    private LineChart cpuChart;
     public void handleCPUSelectionChanged(Event event) {
+        int x = 0,y=1;
+        int s = x+y;
+        System.out.println();
+
     }
 
     public void handleSelectFile(ActionEvent actionEvent) {
