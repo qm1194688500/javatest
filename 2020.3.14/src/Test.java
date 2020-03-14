896187b1365c837e9c3b23d0aa51c2bee2b138b5
qm1194688500 <1194688500@qq.com>
2020-03-14 22:36:53 +0800

    练习

---
+++ 2020.3.14/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        String x="fmn";
+        x.toUpperCase();
+        String y=x.replace('f','F');
+        y=y+"wxy";
+        System.out.println(y);
+    }
+}
