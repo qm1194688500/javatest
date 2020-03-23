a2866a670c643b62bed8e664f9354197214907bc
qm1194688500 <1194688500@qq.com>
2020-03-23 22:49:30 +0800

    练习

---
+++ 2020.3.23/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        boolean[] array = new boolean[p.length];
+        for (int i = 0; i <p.length ; i++) {
+            if (s.contains(p[i])){
+                array[i]=true;
+            }else{
+                array[i]=false;
+            }
+        }
+        return array;
+    }
+}
