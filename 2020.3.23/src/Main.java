01908a450c4f67589a79074cfa17c58e1f0babdf
qm1194688500 <1194688500@qq.com>
2020-03-25 21:29:07 +0800

    ，，

--- 2020.3.23/src/Main.java
+++ 2020.3.23/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
+
     }
     public boolean[] chkSubStr(String[] p, int n, String s) {
         boolean[] array = new boolean[p.length];
