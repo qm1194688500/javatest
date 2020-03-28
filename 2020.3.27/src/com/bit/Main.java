5d4fdc2b5059c42e67194af7aacab93dd30a8812
qm1194688500 <1194688500@qq.com>
2020-03-28 22:45:11 +0800

    练习

---
+++ 2020.3.27/src/com/bit/Main.java
@@ -0,0 +1,16 @@
+package com.bit;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        int num1 = str1.length();
+        int num2 = str2.length();
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+
+}
