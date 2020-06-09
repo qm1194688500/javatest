4e5b45bd1dbd99ae6d269a2dc5aa293f0f6c4c11
qm1194688500 <1194688500@qq.com>
2020-06-09 22:51:23 +0800

    练习

--- javaee-project/springstudy/src/main/java/frank/Main.java
+++ javaee-project/springstudy/src/main/java/frank/Main.java
@@ -9,10 +9,17 @@ public class Main {
         ApplicationContext context = new
                 ClassPathXmlApplicationContext("applications.xml");
        Duck duck1 = (Duck) context.getBean("duck1");
-        System.out.println(duck1);
+        Duck duck3 = (Duck) context.getBean("duck1");
+        System.out.println(duck1==duck3);
         Duck duck2 = (Duck) context.getBean("duck2");
-        System.out.println(duck2);
+        Duck duck4 = (Duck) context.getBean("duck2");
+        System.out.println(duck2==duck4);
         DuckShop shop = (DuckShop) context.getBean("duckShop");
         System.out.println(shop);
+        Duck duck5 = (Duck) context.getBean("duck3");
+        System.out.println(duck5);
+        Duck duck6 = (Duck) context.getBean("duck4");
+        System.out.println(duck6);
+
     }
 }
