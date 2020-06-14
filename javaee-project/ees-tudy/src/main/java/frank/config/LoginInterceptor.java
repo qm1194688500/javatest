5ca93818ef65a856f813b101f22e453252c048c1
qm1194688500 <1194688500@qq.com>
2020-06-14 23:05:23 +0800

    Spring练习

---
+++ javaee-project/ees-tudy/src/main/java/frank/config/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package frank.config;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+        if (session!=null){
+            //权限校验
+            return true;
+        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());//401
+        response.sendRedirect("/login.html");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
--- javaee-project/ees-tudy/src/main/java/frank/controller/LoginController.java
+++ javaee-project/ees-tudy/src/main/java/frank/controller/LoginController.java
@@ -2,15 +2,21 @@ package frank.controller;
 
 import frank.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
+@RequestMapping("/1")
 public class LoginController {
     @RequestMapping("/login")
     public String login(){
-        return "login.html";
+        return "/login.html";
     }
     @RequestMapping(value = "/login2",method = RequestMethod.POST)
     @ResponseBody
@@ -20,4 +26,26 @@ public class LoginController {
         user.setPassword("123");
         return user;
     }
+    @RequestMapping(value = "/login3",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login3(@RequestBody User u){
+        System.out.println(u);
+        User user = new User();
+        user.setUsername("haha");
+        user.setPassword("123");
+        return user;
+    }
+    @RequestMapping(value = "/login4",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login4(HttpServletRequest request, HttpServletResponse response,User u){
+        if ("abc".equals(u.getUsername())&&"123".equals(u.getPassword())){
+            HttpSession session = request.getSession();
+            session.setAttribute("user",u);
+        }
+        User user = new User();
+        user.setUsername("haha");
+        user.setPassword("123");
+        return user;
+    }
+
 }
